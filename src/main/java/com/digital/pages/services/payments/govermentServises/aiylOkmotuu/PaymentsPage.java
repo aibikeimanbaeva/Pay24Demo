be02c6b65903e755babf99bc193e91791ae822cf
tansuluut <tansuluu.totosheva00@gmail.com>
2023-11-29 13:48:05 +0600

    feature:fixed pages and tests

---
+++ src/main/java/com/digital/pages/services/payments/govermentServises/aiylOkmotuu/PaymentsPage.java
@@ -0,0 +1,16 @@
+package com.digital.pages.services.payments.govermentServises.aiylOkmotuu;
+
+
+import com.digital.pages.BasePage;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class PaymentsPage  extends BasePage {
+    @FindBy(xpath = "//div[text()='Государственные услуги и штрафы']")
+    public WebElement governmentServicesCategory;
+
+    public PaymentsPage openGovernmentServicesCategory(){
+        elementActions.clickElement(governmentServicesCategory);
+        return this;
+    }
+}
